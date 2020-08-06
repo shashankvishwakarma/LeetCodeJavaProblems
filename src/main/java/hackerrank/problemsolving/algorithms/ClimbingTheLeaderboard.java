bb7c7603a815e5ee16a897e0e499c6c7b7f05881
Shashank <mail2shashank1683@gmail.com>
2020-08-06 22:40:07 +0530

    added solution

--- src/main/java/hackerrank/problemsolving/algorithms/ClimbingTheLeaderboard.java
+++ src/main/java/hackerrank/problemsolving/algorithms/ClimbingTheLeaderboard.java
@@ -6,23 +6,58 @@ import java.util.Scanner;
 public class ClimbingTheLeaderboard {
     // Complete the climbingLeaderboard function below.
     static int[] climbingLeaderboard(int[] scores, int[] alice) {
-        int si = 0; // score index
-        int rank = 1;
+        int n = scores.length;
+        int m = alice.length;
 
-        for (int x = alice.length - 1; x >= 0; x--) {
-            if (si == scores.length) {
-                rank++;
-                si++;
+        int res[] = new int[m];
+        int[] rank = new int[n];
+
+        rank[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (scores[i] == scores[i - 1]) {
+                rank[i] = rank[i - 1];
+            } else {
+                rank[i] = rank[i - 1] + 1;
             }
-            while (si < scores.length && scores[si] > alice[x]) {
-                if (!(si < scores.length - 1 && scores[si] == scores[si + 1])) {
-                    rank++;
-                }
-                si++;
-            }
-            alice[x] = rank;
         }
-        return alice;
+
+        for (int i = 0; i < m; i++) {
+            int aliceScore = alice[i];
+            if (aliceScore > scores[0]) {
+                res[i] = 1;
+            } else if (aliceScore < scores[n - 1]) {
+                res[i] = rank[n - 1] + 1;
+            } else {
+                int index = binarySearch(scores, aliceScore);
+                res[i] = rank[index];
+
+            }
+        }
+        return res;
+
+    }
+
+    private static int binarySearch(int[] a, int key) {
+
+        int lo = 0;
+        int hi = a.length - 1;
+
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] == key) {
+                return mid;
+            } else if (a[mid] < key && key < a[mid - 1]) {
+                return mid;
+            } else if (a[mid] > key && key >= a[mid + 1]) {
+                return mid + 1;
+            } else if (a[mid] < key) {
+                hi = mid - 1;
+            } else if (a[mid] > key) {
+                lo = mid + 1;
+            }
+        }
+        return -1;
     }
 
 
