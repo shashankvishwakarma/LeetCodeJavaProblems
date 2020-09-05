9ea2edac19199848c3119d17012d13d2f732859d
Shashank <mail2shashank1683@gmail.com>
2020-09-05 17:25:12 +0530

    added solution for FindDigits

---
+++ src/main/java/hackerrank/practice/algorithms/implementation/FindDigits.java
@@ -0,0 +1,37 @@
+package hackerrank.practice.algorithms.implementation;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class FindDigits {
+    // Complete the findDigits function below.
+    static int findDigits(int n) {
+        String numberString = String.valueOf(n);
+        int numberInteger = Integer.parseInt(numberString);
+        int evenlyDivisibleDigits = 0;
+
+        for (int j = 0; j < numberString.length(); j++) {
+            int digit = Character.getNumericValue(numberString.charAt(j));
+            if (digit != 0 && numberInteger % digit == 0) {
+                evenlyDivisibleDigits++;
+            }
+        }
+        return evenlyDivisibleDigits;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            int result = findDigits(n);
+        }
+        scanner.close();
+    }
+}
+
+}
