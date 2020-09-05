08aacd7cddb051a9f9e8e22f35dd586e9ed450a7
Shashank <mail2shashank1683@gmail.com>
2020-09-05 16:17:56 +0530

    added solution for SaveThePrisoner

---
+++ src/main/java/hackerrank/problemsolving/algorithms/SaveThePrisoner.java
@@ -0,0 +1,34 @@
+package hackerrank.problemsolving.algorithms;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SaveThePrisoner {
+    // Complete the saveThePrisoner function below.
+    static int saveThePrisoner(int n, int m, int s) {
+        int poisonedPrisoner = 0;
+        for (int i = 0; i < n; i++) {
+            poisonedPrisoner = (s + m - 1) % n;
+            if (poisonedPrisoner == 0) {
+                poisonedPrisoner = n;
+            }
+        }
+        return poisonedPrisoner;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] nms = scanner.nextLine().split(" ");
+            int n = Integer.parseInt(nms[0]);
+            int m = Integer.parseInt(nms[1]);
+            int s = Integer.parseInt(nms[2]);
+            int result = saveThePrisoner(n, m, s);
+        }
+        scanner.close();
+    }
+}
