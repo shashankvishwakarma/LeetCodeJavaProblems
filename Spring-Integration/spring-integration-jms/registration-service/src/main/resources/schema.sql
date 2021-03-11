78a0d2e819f60f8aa55d66b8e8466c767dad081e
Shashank <mail2shashank1683@gmail.com>
2021-03-11 09:20:20 +0530

    cleanup

--- Spring-Integration/spring-integration-jms/registration-service/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE registration
-(
-    id  varchar(80) NOT NULL PRIMARY KEY,
-    firstname   varchar(30) NOT NULL,
-    lastname    varchar(30) NOT NULL,
-    country      varchar(80) NOT NULL,
-    message      varchar(255) NULL
-);
