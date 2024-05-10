169e4b5aafff044d477e3ea9504b76ae2a3f5387
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-05-10 10:03:49 +0530

    LeetCode: KthLargestElement

---
+++ src/main/java/leetcode/KthLargestElement.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+ */
+public class KthLargestElement {
+
+  /*
+    Input: nums = [3,2,1,5,6,4], k = 2
+    Output: 5
+    */
+  public static void main(String[] args) {
+    int[] nums = new int[] {3,2,1,5,6,4};
+    int k = 2;
+
+    int kthLargest = findKthLargest(nums, k);
+    System.out.println("The " + k + "th largest element is: " + kthLargest);
+  }
+
+  public static int findKthLargest(int[] nums, int k) {
+    Arrays.sort(nums);
+    return nums[nums.length - k];
+  }
+
+}
