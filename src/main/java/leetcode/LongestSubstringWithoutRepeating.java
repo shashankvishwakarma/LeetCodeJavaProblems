5a08ef3d1e47fe95d405354e45b2ac538a62a080
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-05-20 22:02:09 +0530

    LongestSubstringWithoutRepeating

---
+++ src/main/java/leetcode/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ *
+ * Example 1:
+ *
+ * Input: s = "abcabcbb"
+ * Output: 3
+ * Explanation: The answer is "abc", with the length of 3.
+ * Example 2:
+ *
+ * Input: s = "bbbbb"
+ * Output: 1
+ * Explanation: The answer is "b", with the length of 1.
+ * Example 3:
+ *
+ * Input: s = "pwwkew"
+ * Output: 3
+ * Explanation: The answer is "wke", with the length of 3.
+ * Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
+public class LongestSubstringWithoutRepeating {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+
+        for (char c: s.toCharArray()) {
+
+        }
+
+    }
+}
