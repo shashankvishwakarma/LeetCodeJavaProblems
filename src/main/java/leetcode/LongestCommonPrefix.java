e2807449e852d25c17edd79075caa9dd45ac95d8
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-05-21 06:43:54 +0530

    LongestCommonPrefix

---
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-common-prefix/
+ *
+ * Example 1:
+ *
+ * Input: strs = ["flower","flow","flight"]
+ * Output: "fl"
+ * Example 2:
+ *
+ * Input: strs = ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ */
+public class LongestCommonPrefix {
+}
---
+++ src/main/java/leetcode/LongestPalindromicSubstring.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/longest-palindromic-substring/description/
+ *
+ * Example 1:
+ *
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: s = "cbbd"
+ * Output: "bb"
+ */
+public class LongestPalindromicSubstring {
+}
