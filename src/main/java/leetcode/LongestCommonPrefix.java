f80ef771b8b68c6df494f557317e3ef2036a846b
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-05-26 11:12:08 +0530

    LongestCommonPrefix

--- src/main/java/leetcode/LongestCommonPrefix.java
+++ src/main/java/leetcode/LongestCommonPrefix.java
@@ -3,6 +3,7 @@ package leetcode;
 /**
  * https://leetcode.com/problems/longest-common-prefix/
  *
+ * <pre>
  * Example 1:
  *
  * Input: strs = ["flower","flow","flight"]
@@ -12,6 +13,31 @@ package leetcode;
  * Input: strs = ["dog","racecar","car"]
  * Output: ""
  * Explanation: There is no common prefix among the input strings.
+ * </pre>
  */
 public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        String longestCommonPrefix = longestCommonPrefix(strs);
+        System.out.println("Longest Common Prefix: " + longestCommonPrefix);
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+
+        return prefix;
+    }
 }
