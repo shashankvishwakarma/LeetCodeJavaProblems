0c7ab9c41a14aaf26a02475562e475e7678de8ce
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-06-30 18:40:53 +0530

    BinarySearch

---
+++ src/main/java/dsa/BinarySearch.java
@@ -0,0 +1,31 @@
+package dsa;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length;
+
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target == arr[mid]) {
+                return mid;
+            }
+
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                end = end - 1;
+            }
+        }
+
+        return -1;
+    }
+}
