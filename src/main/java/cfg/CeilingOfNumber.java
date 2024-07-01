27b49806a927b63c3d2785af04483df6f015587b
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-01 06:18:21 +0530

    CeilingOfNumber

---
+++ src/main/java/cfg/CeilingOfNumber.java
@@ -0,0 +1,57 @@
+package cfg;
+
+public class CeilingOfNumber {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        int ans = ceiling1(arr, target);
+        System.out.println(ans);
+    }
+
+    // return the index of smallest no >= target
+    static int ceiling(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        int possible = arr[start];
+
+        if (target > arr[end]) {
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            possible = arr[mid];
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target == arr[mid]) {
+                return target;
+            }
+        }
+        return possible;
+    }
+
+    // return the index of smallest no >= target
+    static int ceiling1(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        if (target > arr[end]) {
+            return -1;
+        }
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target == arr[mid]) {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
