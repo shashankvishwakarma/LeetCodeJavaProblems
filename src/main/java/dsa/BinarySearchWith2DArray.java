966630685407a14331d45b5e32e079fd362f8a75
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-05 22:02:03 +0530

    BinarySearch With 2D Array

---
+++ src/main/java/dsa/BinarySearchWith2DArray.java
@@ -0,0 +1,36 @@
+package dsa;
+
+import java.util.Arrays;
+
+public class BinarySearchWith2DArray {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {10, 20, 30, 40},
+                {15, 25, 35, 45},
+                {28, 29, 37, 49},
+                {33, 34, 38, 50}
+        };
+
+        System.out.println(Arrays.toString(search(arr, 49)));
+        System.out.println(Arrays.toString(search(arr, 37)));
+        System.out.println(Arrays.toString(search(arr, 59)));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+
+        int row = 0;
+        int column = matrix[0].length - 1;
+
+        while (row < matrix.length && column >= 0) {
+            if (matrix[row][column] == target) {
+                return new int[]{row, column};
+            } else if (matrix[row][column] < target) {
+                row++;
+            } else {
+                column--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+}
