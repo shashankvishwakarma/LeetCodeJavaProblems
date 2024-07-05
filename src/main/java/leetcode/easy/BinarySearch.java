e7cc1e6376fa9b215256119d9781eb2d3a1ad9b6
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-05 22:22:52 +0530

    BinarySearch

---
+++ src/main/java/leetcode/easy/BinarySearch.java
@@ -0,0 +1,43 @@
+package leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/binary-search/
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 9
+ * Output: 4
+ * Explanation: 9 exists in nums and its index is 4
+ * Example 2:
+ * <p>
+ * Input: nums = [-1,0,3,5,9,12], target = 2
+ * Output: -1
+ * Explanation: 2 does not exist in nums so return -1
+ */
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int target = 22;
+        int ans = search(arr, target);
+        System.out.println(ans);
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
