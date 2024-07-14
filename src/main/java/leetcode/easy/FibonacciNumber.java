a3668643d56a0d887c124a8cdcef25c3a88b0364
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-14 18:49:18 +0530

    Fibonacci number

--- src/main/java/leetcode/easy/FibonacciNumber.java
+++ src/main/java/leetcode/easy/FibonacciNumber.java
@@ -5,14 +5,23 @@ package leetcode.easy;
  */
 public class FibonacciNumber {
     public static void main(String[] args) {
-        int ans = fib(4);
+        long ans = fib(50);
         System.out.println(ans);
     }
 
-    public static int fib(int n) {
+    public static long fib(int n) {
         if (n < 2) {
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+
+        long a = 0;
+        long b = 1;
+        long c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
     }
 }
