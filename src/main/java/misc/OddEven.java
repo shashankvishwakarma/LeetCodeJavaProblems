b13913795a910dbfec89d34fb4e2754cf9e54b52
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-17 16:59:12 +0530

    check even/odd number using bitwise operator

---
+++ src/main/java/misc/OddEven.java
@@ -0,0 +1,12 @@
+package misc;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int num = 66;
+        System.out.println(isOdd(num));
+    }
+
+    private static boolean isOdd(int n) {
+        return (n & 1) == 1;
+    }
+}
