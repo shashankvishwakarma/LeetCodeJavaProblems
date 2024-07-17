3f085c1c4e20bff1515465a81a6c07e9f696ee86
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-17 18:15:30 +0530

    Find no of digits using bitwise operator

---
+++ src/main/java/misc/NoOfDigits.java
@@ -0,0 +1,17 @@
+package misc;
+
+/**
+ * Find ith bit of the number
+ */
+public class NoOfDigits {
+    public static void main(String[] args) {
+        int num = 10;
+        int base = 2;
+        System.out.println("No Of Digits in Decimal representation:" + noOfDigits(num, 10));
+        System.out.println("No Of Digits in binary representation:" + noOfDigits(num, base));
+    }
+
+    private static int noOfDigits(int num, int base) {
+        return (int) (Math.log(num) / Math.log(base)) + 1;
+    }
+}
