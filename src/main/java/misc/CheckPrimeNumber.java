e432a5eeb089403e5f8fb1cd6cb8cc7f4288c89d
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-18 21:58:34 +0530

    check prime number with optimization

--- src/main/java/misc/CheckPrimeNumber.java
+++ src/main/java/misc/CheckPrimeNumber.java
@@ -1,14 +1,41 @@
 package misc;
 
 public class CheckPrimeNumber {
+
     public static void main(String[] args) {
-        int n = 20;
+        int n = 40;
+        //checkPrimeNumber(n);
+        checkPrimeNumberOptimized(n);
+    }
+
+    private static void checkPrimeNumberOptimized(int n) {
+        boolean[] primes = new boolean[n + 1];
+        sieve(n, primes);
+    }
+
+    static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    private static void checkPrimeNumber(int n) {
         for (int i = 2; i <= n; i++) {
             System.out.println(i + " " + isPrime(i));
         }
     }
 
-    static boolean isPrime(int n) {
+    private static boolean isPrime(int n) {
         if (n <= 1) {
             return false;
         }
