166eebaf991379fe53a18c1da90f43760255c7bb
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-20 17:27:08 +0530

    Quick Sort Algorithm

---
+++ src/main/java/dsa/sorting/QuickSortAlgorithm.java
@@ -0,0 +1,44 @@
+package dsa.sorting;
+
+import java.util.Arrays;
+
+public class QuickSortAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] nums, int low, int high) {
+
+        if (low >= high) {
+            return;
+        }
+
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = nums[mid];
+
+        while (start <= end) {
+
+            while (nums[start] < pivot) {
+                start++;
+            }
+            while (nums[end] > pivot) {
+                end--;
+            }
+
+            if (start <= end) {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = temp;
+                start++;
+                end--;
+            }
+        }
+
+        sort(nums, low, end);
+        sort(nums, start, high);
+    }
+}
