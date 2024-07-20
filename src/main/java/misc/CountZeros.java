de7d8fa8aeb8ea3912fa1a6a45abc0e9080a0d26
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-20 11:56:33 +0530

    count no of zeros for given number

---
+++ src/main/java/misc/CountZeros.java
@@ -0,0 +1,23 @@
+package misc;
+
+public class CountZeros {
+
+    static int counter = 0;
+
+    public static void main(String[] args) {
+        countZeros(30210004);
+        System.out.println(counter);
+    }
+
+    private static int countZeros(int n) {
+        if (n == 0) {
+            return counter;
+        }
+
+        int rem = n % 10;
+        if (rem == 0) {
+            counter++;
+        }
+        return countZeros(n / 10);
+    }
+}
