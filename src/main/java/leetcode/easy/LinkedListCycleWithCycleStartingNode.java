644661d763a377c1fd74b10ebc86d53835f3e7d5
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-25 22:16:31 +0530

    Linked List Cycle with returning the node where the cycle begins.

---
+++ src/main/java/leetcode/easy/LinkedListCycleWithCycleStartingNode.java
@@ -0,0 +1,63 @@
+package leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle-ii/
+ */
+public class LinkedListCycleWithCycleStartingNode {
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        // find the start node
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        // keep moving both forward and they will meet at cycle start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                // calculate the length
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+}
