6ededacadef83def1ab8caebb38a15a80ba3d688
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-25 22:32:49 +0530

    Happy Number

---
+++ src/main/java/leetcode/easy/HappyNumber.java
@@ -0,0 +1,33 @@
+package leetcode.easy;
+
+public class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(19));
+        System.out.println(isHappy(2));
+    }
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int findSquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+}
