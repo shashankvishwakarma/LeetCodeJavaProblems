dbf946640425139291cfd463c281685dce7cd54e
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-25 21:54:05 +0530

    Linked List Cycle length

---
+++ src/main/java/misc/LinkedListCycleLength.java
@@ -0,0 +1,24 @@
+package misc;
+
+public class LinkedListCycleLength {
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                // calculate the length
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+}
