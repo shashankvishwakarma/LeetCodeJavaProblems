2de901f72d8b2ad84116b0714206c9c9560817c5
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-26 21:46:48 +0530

    Middle of the Linked List

---
+++ src/main/java/leetcode/easy/MiddleOfTheLinkedList.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+public class MiddleOfTheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
