3e1ba88593657ddf81a080cbdfa16705ca51f7c1
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-26 22:24:43 +0530

    Reverse Linked List

---
+++ src/main/java/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode present = head;
+        ListNode next = present.next;
+
+        while (present != null) {
+            present.next = prev;
+            prev = present;
+            present = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+        return prev;
+    }
+}
