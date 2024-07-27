bb90d01fc9b3f423628b3ec48a058569de40f37f
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 13:51:26 +0530

    Rotate List

---
+++ src/main/java/leetcode/medium/RotateList.java
@@ -0,0 +1,34 @@
+package leetcode.medium;
+
+import leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/rotate-list/description/
+ */
+public class RotateList {
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (k <= 0 || head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode last = head;
+        int length = 1;
+        while (last.next != null) {
+            last = last.next;
+            length++;
+        }
+
+        last.next = head;
+        int rotations = k % length;
+        int skip = length - rotations;
+        ListNode newLast = head;
+        for (int i = 0; i < skip - 1; i++) {
+            newLast = newLast.next;
+        }
+        head = newLast.next;
+        newLast.next = null;
+
+        return head;
+    }
+}
