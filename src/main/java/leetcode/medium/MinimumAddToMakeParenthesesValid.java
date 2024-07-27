207e9ee6af21c9d0d70efc7bbe260acc1789a0c6
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 23:23:27 +0530

    Minimum Add to Make Parentheses Valid

--- src/main/java/leetcode/medium/MinimumAddToMakeParenthesesValid.java
+++ src/main/java/leetcode/medium/MinimumAddToMakeParenthesesValid.java
@@ -5,7 +5,7 @@ import java.util.Stack;
 /**
  * https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/description/
  */
-public class MinimumInsertionToMakeParenthesesValid {
+public class MinimumAddToMakeParenthesesValid {
     public static void main(String[] args) {
         String input = "()))((";
         System.out.println(minAddToMakeValid(input));
