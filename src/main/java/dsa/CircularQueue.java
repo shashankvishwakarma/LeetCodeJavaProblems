369cfd10764052c1eac86c8940cb94f4d426d878
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 19:00:54 +0530

    Circular Queue Custom Implementation

---
+++ src/main/java/dsa/CircularQueue.java
@@ -0,0 +1,89 @@
+package dsa;
+
+public class CircularQueue {
+    protected int[] data;
+    private static final int DEFAULT_SIZE = 10;
+
+    protected int front = 0;
+    protected int end = 0;
+    private int size = 0;
+
+    public CircularQueue() {
+        this(DEFAULT_SIZE);
+    }
+
+    public CircularQueue(int size) {
+        this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return size == data.length;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[end++] = item;
+        end = end % data.length;
+        size++;
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+
+        int removed = data[front++];
+        front = front % data.length;
+        size--;
+        return removed;
+    }
+
+    public int front() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+        return data[front];
+    }
+
+    public void display() {
+        if (isEmpty()) {
+            System.out.println("Empty");
+            return;
+        }
+        int i = front;
+        do {
+            System.out.println(i);
+            System.out.print(data[i] + " -> ");
+            i++;
+            i %= data.length;
+        } while (i != end);
+        System.out.println("END");
+    }
+
+    public static void main(String[] args) throws Exception {
+        CircularQueue queue = new CircularQueue(5);
+        queue.insert(3);
+        queue.insert(6);
+        queue.insert(5);
+        queue.insert(19);
+        queue.insert(1);
+
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.insert(133);
+        queue.display();
+
+        System.out.println(queue.remove());
+        queue.insert(99);
+        queue.display();
+
+    }
+}
