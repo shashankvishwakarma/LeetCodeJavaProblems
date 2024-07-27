f245723d3a830fc662d221596d26416761051302
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 13:34:06 +0530

    Reverse alternate K nodes in a Singly Linked List

---
+++ src/main/java/geeksforgeeks/ReverseAlternateKNodesInSinglyLinkedList.java
@@ -0,0 +1,49 @@
+package geeksforgeeks;
+
+import leetcode.ListNode;
+
+/**
+ * https://www.geeksforgeeks.org/reverse-alternate-k-nodes-in-a-singly-linked-list/
+ */
+public class ReverseAlternateKNodesInSinglyLinkedList {
+    public ListNode reverseAlternateKGroup(ListNode head, int k) {
+        if (k <= 1 || head == null) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+
+        while (current != null) {
+            ListNode last = prev;
+            ListNode newEnd = current;
+
+            // reverse between left and right
+            ListNode next = current.next;
+            for (int i = 0; current != null && i < k; i++) {
+                current.next = prev;
+                prev = current;
+                current = next;
+                if (next != null) {
+                    next = next.next;
+                }
+            }
+
+            if (last != null) {
+                last.next = prev;
+            } else {
+                head = prev;
+            }
+
+            newEnd.next = current;
+
+            // skip the k nodes
+            for (int i = 0; current != null && i < k; i++) {
+                prev = current;
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
