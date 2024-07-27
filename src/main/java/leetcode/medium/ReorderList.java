a333c959c159292683fca839ddc57498060017aa
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 13:16:29 +0530

    Reorder List

---
+++ src/main/java/leetcode/medium/ReorderList.java
@@ -0,0 +1,39 @@
+package leetcode.medium;
+
+import leetcode.ListNode;
+
+import static leetcode.easy.MiddleOfTheLinkedList.middleNode;
+import static leetcode.easy.ReverseLinkedList.reverseList;
+
+/**
+ * https://leetcode.com/problems/reorder-list/description/
+ */
+public class ReorderList {
+
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        ListNode mid = middleNode(head);
+
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
+
+        // rearrange
+        while (hf != null && hs != null) {
+            ListNode temp = hf.next;
+            hf.next = hs;
+            hf = temp;
+
+            temp = hs.next;
+            hs.next = hf;
+            hs = temp;
+        }
+
+        // next of tail to null
+        if (hf != null) {
+            hf.next = null;
+        }
+    }
+}
