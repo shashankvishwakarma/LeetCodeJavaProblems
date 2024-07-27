04ea29542d03e0e6ca56ec858ed5e48c31b446ea
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 12:44:37 +0530

    Reverse Linked List segment

---
+++ src/main/java/leetcode/medium/ReverseLinkedListSegment.java
@@ -0,0 +1,46 @@
+package leetcode.medium;
+
+import leetcode.ListNode;
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list-ii/
+ */
+public class ReverseLinkedListSegment {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (left == right) {
+            return head;
+        }
+
+        // skip the first left-1 nodes
+        ListNode current = head;
+        ListNode prev = null;
+        for (int i = 0; current != null && i < left - 1; i++) {
+            prev = current;
+            current = current.next;
+        }
+
+        ListNode last = prev;
+        ListNode newEnd = current;
+
+        // reverse between left and right
+        ListNode next = current.next;
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            if (next != null) {
+                next = next.next;
+            }
+        }
+
+        if (last != null) {
+            last.next = prev;
+        } else {
+            head = prev;
+        }
+
+        newEnd.next = current;
+        return head;
+    }
+
+}
