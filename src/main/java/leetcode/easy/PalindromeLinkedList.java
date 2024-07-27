eca7eb5704832d2afc6d87226361efcf642e6ffb
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-27 13:03:53 +0530

    Palindrome Linked List

---
+++ src/main/java/leetcode/easy/PalindromeLinkedList.java
@@ -0,0 +1,29 @@
+package leetcode.easy;
+
+import leetcode.ListNode;
+
+import static leetcode.easy.MiddleOfTheLinkedList.middleNode;
+import static leetcode.easy.ReverseLinkedList.reverseList;
+
+/**
+ * https://leetcode.com/problems/palindrome-linked-list/description/
+ */
+public class PalindromeLinkedList {
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode headSecond = reverseList(mid);
+        ListNode rereverseHead = headSecond;
+
+        // compare both the halves
+        while (head != null && headSecond != null) {
+            if (head.val != headSecond.val) {
+                break;
+            }
+            head = head.next;
+            headSecond = headSecond.next;
+        }
+        reverseList(rereverseHead);
+
+        return head == null || headSecond == null;
+    }
+}
