62383c060d704e56b969243157bc687cbbda51ac
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-28 19:18:49 +0530

    Binary search Tree implementation

---
+++ src/main/java/dsa/trees/BinarySearchTree.java
@@ -0,0 +1,78 @@
+package dsa.trees;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    public class Node {
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public void insert(int value) {
+        root = insert(value, root);
+    }
+
+    public Node insert(int value, Node node) {
+        if (node == null) {
+            node = new Node(value);
+            return node;
+        }
+
+        if (value < node.value) {
+            node.left = insert(value, node.left);
+        }
+
+        if (value > node.value) {
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return node;
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+        int[] nums = {5, 2, 7, 1, 4, 6, 9, 8, 3, 10};
+
+        for (int i = 0; i < nums.length; i++) {
+            binarySearchTree.insert(nums[i]);
+        }
+        binarySearchTree.display();
+    }
+}
