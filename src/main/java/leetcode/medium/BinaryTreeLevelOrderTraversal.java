a37b10a2e759f15630e213b8ca5f077f6f65a6c4
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-07-31 21:58:55 +0530

    Binary Tree Level Order Traversal

--- src/main/java/leetcode/medium/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/leetcode/medium/BinaryTreeLevelOrderTraversal.java
@@ -56,24 +56,26 @@ public class BinaryTreeLevelOrderTraversal {
         }
         return result;
     }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 }
 
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
 
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
