1be1856abb50fbb6433234b75d1fa413ff9def6a
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-01 21:23:28 +0530

    Populating Next Right Pointers in Each Node

---
+++ src/main/java/leetcode/medium/PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,61 @@
+package leetcode.medium;
+
+/**
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/description/
+ */
+public class PopulatingNextRightPointersInEachNode {
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node result = connect(root);
+
+    }
+
+    public static Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        Node leftMost = root;
+        while (leftMost.left != null) {
+            Node current = leftMost;
+            while (current != null) {
+                current.left.next = current.right;
+                if (current.next != null) {
+                    current.right.next = current.next.left;
+                }
+                current = current.next;
+            }
+            leftMost = leftMost.left;
+        }
+        return root;
+    }
+
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int val) {
+            this.val = val;
+        }
+
+        public Node(int val, Node left, Node right, Node next) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            this.next = next;
+        }
+    }
+}
