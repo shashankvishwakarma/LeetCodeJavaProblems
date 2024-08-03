e84d9a5cfc395f0fb3f533f5d26cf918f18f9613
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-03 16:28:14 +0530

    Lowest Common Ancestor of a Binary Tree

---
+++ src/main/java/leetcode/medium/LowestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,53 @@
+package leetcode.medium;
+
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
+ */
+public class LowestCommonAncestorOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(6);
+        root.left.right = new TreeNode(2);
+        root.left.right.left = new TreeNode(7);
+        root.left.right.right = new TreeNode(4);
+        root.right.left = new TreeNode(0);
+        root.right.right = new TreeNode(8);
+
+        System.out.println(lowestCommonAncestor(root, new TreeNode(5), new TreeNode(1)).val); //expected 3
+        System.out.println(lowestCommonAncestor(root, new TreeNode(5), new TreeNode(4)).val); //expected 5
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+
+        return left == null ? right : left;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
+
+
+
