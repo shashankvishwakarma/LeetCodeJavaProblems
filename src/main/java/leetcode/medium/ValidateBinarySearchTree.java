2a0bcb114b9617f961128a3a2b0b9bc6eab7150f
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-03 11:46:09 +0530

    Validate Binary Search Tree

---
+++ src/main/java/leetcode/medium/ValidateBinarySearchTree.java
@@ -0,0 +1,62 @@
+package leetcode.medium;
+
+/**
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ */
+public class ValidateBinarySearchTree {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(3);
+        root.right.right = new TreeNode(6);
+
+        System.out.println(isValidBST(root));
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    public static boolean isValidBST(TreeNode node, Integer min, Integer max) {
+        if (node == null) {
+            return true;
+        }
+
+        if (min != null && node.val <= min) {
+            return false;
+        }
+
+        if (max != null && node.val >= max) {
+            return false;
+        }
+
+        //return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
+        boolean leftTree = isValidBST(node.left, min, node.val);
+        boolean rightTree = isValidBST(node.right, node.val, max);
+
+        return leftTree && rightTree;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
+
