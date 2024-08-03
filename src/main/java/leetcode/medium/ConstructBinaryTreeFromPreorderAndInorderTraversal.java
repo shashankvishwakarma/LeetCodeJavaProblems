d0ab1c3b0f448bc4f5fe9ab3a32dd1cd9623e6b2
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-03 17:04:35 +0530

    Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/leetcode/medium/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,59 @@
+package leetcode.medium;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+
+        System.out.println(buildTree(preorder, inorder)); //expected output {3,9,20,null,null,15,7}
+    }
+
+    public static TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0) {
+            return null;
+        }
+
+        int r = preorder[0];
+        int index = 0;
+
+        for (int i = 0; i < inorder.length; i++) {
+            if (inorder[i] == r) {
+                index = i;
+            }
+        }
+
+        TreeNode node = new TreeNode(r);
+
+        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index + 1), Arrays.copyOfRange(inorder, 0, index));
+        node.right = buildTree(Arrays.copyOfRange(preorder, index + 1, preorder.length), Arrays.copyOfRange(inorder, index + 1, inorder.length));
+
+        return node;
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
+
