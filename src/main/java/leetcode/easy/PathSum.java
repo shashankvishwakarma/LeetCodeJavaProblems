7ac2ab16c41b802e5972e64e8fa851b8c1a14d67
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-03 18:26:20 +0530

    Path Sum

---
+++ src/main/java/leetcode/easy/PathSum.java
@@ -0,0 +1,51 @@
+package leetcode.easy;
+
+/**
+ * https://leetcode.com/problems/path-sum/description/
+ */
+public class PathSum {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(8);
+        root.left.left = new TreeNode(11);
+        root.left.left.left = new TreeNode(7);
+        root.left.left.right = new TreeNode(2);
+        root.right.left = new TreeNode(13);
+        root.right.right = new TreeNode(4);
+        root.right.right.right = new TreeNode(1);
+
+        int targetSum = 22;
+        System.out.println(hasPathSum(root, targetSum)); // expected output true
+    }
+
+    public static boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        if (root.val == targetSum && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
