b6a3fe225f7f89fbe575a56048662b820ef6f114
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-08-04 14:15:50 +0530

    DFS using stack

---
+++ src/main/java/misc/DepthFirstSearchUsingStack.java
@@ -0,0 +1,51 @@
+package misc;
+
+import java.util.Stack;
+
+/**
+ * Depth first search using stack
+ */
+public class DepthFirstSearchUsingStack {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(12);
+        root.right.left.left = new TreeNode(16);
+        root.right.right.left = new TreeNode(8);
+
+        dfsStack(root);
+    }
+
+    static void dfsStack(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(node);
+
+        while (!stack.isEmpty()) {
+            TreeNode removed = stack.pop();
+            System.out.print(removed.val + " ");
+            if (removed.right != null) {
+                stack.push(removed.right);
+            }
+            if (removed.left != null) {
+                stack.push(removed.left);
+            }
+        }
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+    }
+}
