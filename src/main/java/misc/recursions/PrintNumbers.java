4117f71a700736fc240a1b1a03ea7d4e88435ab8
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-09-17 09:16:01 +0530

    PrintNumbers 1-10

---
+++ src/main/java/misc/recursions/PrintNumbers.java
@@ -0,0 +1,12 @@
+package misc.recursions;
+
+public class PrintNumbers {
+
+  public static void main(String[] args) {
+    printNumbers(10);
+  }
+
+  private static void printNumbers(int n) {
+
+  }
+}
