cd64eeaf08e01103d12346ed345cf152f666014a
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2024-09-17 11:18:45 +0530

    Print Sum Of First N Natural Numbers

--- src/main/java/misc/recursions/PrintSumOfFirstNNaturalNumbers.java
+++ src/main/java/misc/recursions/PrintSumOfFirstNNaturalNumbers.java
@@ -7,6 +7,7 @@ public class PrintSumOfFirstNNaturalNumbers {
   public static void main(String[] args) {
     printSum(5);
     System.out.println(sum);
+    printSum1(5, 0);
   }
 
   private static void printSum(int n) {
@@ -15,5 +16,14 @@ public class PrintSumOfFirstNNaturalNumbers {
     }
     sum += n;
     printSum(n - 1);
+  }
+
+  private static void printSum1(int n, int sum) {
+    if (n == 0) {
+      System.out.println(sum);
+      return;
+    }
+    sum += n;
+    printSum1(n - 1, sum);
   }
 }
