534256325f69d8744b607afe2167e434eece9bc5
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-12-23 14:30:20 +0530

    Check if an array is sorted (strictly increasing).

--- src/main/java/misc/recursions/CheckIfArrayIsSorted.java
+++ src/main/java/misc/recursions/CheckIfArrayIsSorted.java
@@ -3,14 +3,26 @@ package misc.recursions;
 //Check if an array is sorted (strictly increasing).
 public class CheckIfArrayIsSorted {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 5, 16, 8};
-        System.out.println(isSortedArray(arr, 0));
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 6, 3, 4, 5};
+
+        if (isStrictlyIncreasing(arr1, 0)) {
+            System.out.println("Array 1 is Strictly Increasing");
+        } else {
+            System.out.println("Array 1 is NOT Strictly Increasing");
+        }
+
+        if (isStrictlyIncreasing(arr2, 0)) {
+            System.out.println("Array 2 is Strictly Increasing");
+        } else {
+            System.out.println("Array 2 is NOT Strictly Increasing");
+        }
     }
 
-    static boolean isSortedArray(int[] arr, int index) {
+    static boolean isStrictlyIncreasing(int[] arr, int index) {
         if (index == arr.length - 1)
             return true;
 
-        return arr[index] < arr[index + 1] && isSortedArray(arr, index + 1);
+        return arr[index] < arr[index + 1] && isStrictlyIncreasing(arr, index + 1);
     }
 }
