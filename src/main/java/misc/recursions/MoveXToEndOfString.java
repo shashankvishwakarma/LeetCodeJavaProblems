9febbf9379ac4c46e64de2f0b81db5731b85ace9
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2024-12-23 14:52:10 +0530

    Move all ‘x’ to the end of the string.

---
+++ src/main/java/misc/recursions/MoveXToEndOfString.java
@@ -0,0 +1,29 @@
+package misc.recursions;
+
+//Move all ‘x’ to the end of the string.
+public class MoveXToEndOfString {
+    public static void main(String[] args) {
+        String str = "abcdefxghxixjxxxk";
+        int count = 0;
+        String newStr = "";
+        newStr = moveAllX(str, 0, count, newStr);
+        System.out.println(newStr);
+
+    }
+
+    private static String moveAllX(String str, int index, int count, String newStr) {
+        if (str.length() == index) {
+            for (int i = 0; i < count; i++) {
+                newStr = newStr + 'x';
+            }
+            return newStr;
+        }
+
+        if (str.charAt(index) == 'x') {
+            count++;
+        } else {
+            newStr = newStr + str.charAt(index);
+        }
+        return moveAllX(str, index + 1, count, newStr);
+    }
+}
