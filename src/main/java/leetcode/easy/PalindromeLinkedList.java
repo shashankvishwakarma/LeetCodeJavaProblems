b1086d79866e4c9274c0f24ddbe648544efec26b
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2025-01-02 19:07:32 +0530

    remove linked list elements

--- src/main/java/leetcode/easy/PalindromeLinkedList.java
+++ src/main/java/leetcode/easy/PalindromeLinkedList.java
@@ -1,18 +1,40 @@
 package leetcode.easy;
 
-import dsa.linkedlist.ListNode;
-
 import static leetcode.easy.MiddleOfTheLinkedList.middleNode;
 import static leetcode.easy.ReverseLinkedList.reverseList;
 
+import dsa.linkedlist.ListNode;
+
 /**
  * https://leetcode.com/problems/palindrome-linked-list/description/
+ *
+ * Example 1:
+ * Input: head = [1,2,2,1]
+ * Output: true
+ *
+ * Example 2:
+ * Input: head = [1,2]
+ * Output: false
  */
 public class PalindromeLinkedList {
-    public boolean isPalindrome(ListNode head) {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.insertAtEnd(2);
+        head.insertAtEnd(6);
+        head.insertAtEnd(3);
+        head.insertAtEnd(4);
+        head.insertAtEnd(5);
+        head.insertAtEnd(6);
+
+        head.printList();
+        System.out.println("Linked list is palindrome " + isPalindrome(head));
+    }
+
+    public static boolean isPalindrome(ListNode head) {
         ListNode mid = middleNode(head);
         ListNode headSecond = reverseList(mid);
-        ListNode rereverseHead = headSecond;
+        ListNode reverseHead = headSecond;
 
         // compare both the halves
         while (head != null && headSecond != null) {
@@ -22,7 +44,7 @@ public class PalindromeLinkedList {
             head = head.next;
             headSecond = headSecond.next;
         }
-        reverseList(rereverseHead);
+        reverseList(reverseHead);
 
         return head == null || headSecond == null;
     }
