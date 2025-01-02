a964d6660aff5ab844a3c5d9427519ca9cc2ed6d
Shashank Vishwakarma <150128068+shashank-vishwakarma-rl@users.noreply.github.com>
2025-01-02 17:33:13 +0530

    Reverse LinkedList

--- src/main/java/leetcode/easy/ReverseLinkedList.java
+++ src/main/java/leetcode/easy/ReverseLinkedList.java
@@ -4,8 +4,38 @@ import dsa.linkedlist.ListNode;
 
 /**
  * https://leetcode.com/problems/reverse-linked-list/
+ * <p>
+ * Example 1: Input: head = [1,2,3,4,5] Output: [5,4,3,2,1]
+ * <p>
+ * Example 2: Input: head = [1,2] Output: [2,1]
+ * <p>
+ * Example 3: Input: head = [] Output: []
  */
 public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.insertAtEnd(2);
+        head.insertAtEnd(3);
+        head.insertAtEnd(4);
+        head.insertAtEnd(5);
+        System.out.print("Original List : ");
+        head.printList();
+        ListNode reversedHead = reverseList(head);
+        System.out.print("Reversed List : ");
+        reversedHead.printList(); //Output: [5,4,3,2,1]
+
+        head = new ListNode(1);
+        head.insertAtEnd(2);
+        head.insertAtEnd(3);
+        System.out.print("Original List : ");
+        head.printList();
+        reversedHead = reverseList(head);
+        System.out.print("Reversed List : ");
+        reversedHead.printList();//Output: [2,1]
+    }
+
     public static ListNode reverseList(ListNode head) {
 
         if (head == null) {
