d307073182718845f904c4a5b64446ef71eaf274
Shashank Vishwakarma <mail2shashank1683@gmail.com>
2025-01-14 11:22:31 +0530

    linked list has cycle

--- src/main/java/leetcode/easy/LinkedListCycle.java
+++ src/main/java/leetcode/easy/LinkedListCycle.java
@@ -7,18 +7,28 @@ import dsa.linkedlist.ListNode;
  */
 public class LinkedListCycle {
 
-    public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+  public static void main(String[] args) {
+    ListNode head = new ListNode(3);
+    head.insertAtEnd(2);
+    head.insertAtEnd(0);
+    head.insertAtEnd(4);
+    System.out.print("Original List : ");
+    head.printList();
+    System.out.print("List has cycle : " + hasCycle(head));
+  }
 
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+  public static boolean hasCycle(ListNode head) {
+    ListNode fast = head;
+    ListNode slow = head;
 
-            if (fast == slow) {
-                return true;
-            }
-        }
-        return false;
+    while (fast != null && fast.next != null) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (fast == slow) {
+        return true;
+      }
     }
+    return false;
+  }
 }
